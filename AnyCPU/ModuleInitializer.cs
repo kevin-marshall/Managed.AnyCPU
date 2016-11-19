04c70a03ebe37e5da3f8f910c9a8de730a92a000
kevin-marshall <kevin@marshallville.net>
2016-11-18 17:25:12 -0700

    Improved the logic behind saving the Managed C++ dll to file.

--- AnyCPU/ModuleInitializer.cs
+++ AnyCPU/ModuleInitializer.cs
@@ -16,33 +16,41 @@ namespace AnyCPU
             lock (typeof(ModuleInitializer))
             {
                 System.AppDomain.CurrentDomain.AssemblyResolve += Resolver;
+
+                // ---------------------------------------------------------------------------------
+                // Try to delete the assembly file when the AppDomain is finished with it.
+                //   System.AppDomain.CurrentDomain.ProcessExit += OnExit
+                //   System.AppDomain.CurrentDomain.DomainUnload += OnDomainUnload;
+
+                // Unable to delete file while the assembly is loaded into the domain. The System.AppDomain.ProcessExit event
+                // is not guarenteed to be raised, and System.AppDomain.Unload is too early to delete the file.            
+                // ---------------------------------------------------------------------------------
             }
         }
 
         /// Will attempt to load missing assembly from either x86 or x64 subdir
         internal static System.Reflection.Assembly Resolver(object sender, System.ResolveEventArgs args)
         {
-            string assemblyName = new AssemblyName(args.Name).Name + ".dll";
+            string assembly_name = new AssemblyName(args.Name).Name + ".dll";
 
-            string platform = "x86";
-            if (Environment.Is64BitProcess)
-            {
-                platform = "x64";
-            }
-
-            string resource_path = "AnyCPU.Resources." + platform + "." + assemblyName;
-            string file_path = System.Environment.GetEnvironmentVariable("TEMP") + "\\" + assemblyName;
-
-            if (System.IO.File.Exists(file_path))
-            {
-                System.IO.File.Delete(file_path);
-            }
-
+            string resource_path = "AnyCPU.Resources." + Platform() + "." + assembly_name;
+            string file_path = GetInteropDLLPath(assembly_name);
             using (var dll_resource = System.Reflection.Assembly.GetAssembly(typeof(ModuleInitializer)).GetManifestResourceStream(resource_path))
             {
                 if(dll_resource == null)
                 {
                     return null; 
+                }
+
+                if (System.IO.File.Exists(file_path))
+                {
+                    System.IO.File.Delete(file_path);
+                }
+
+                string directory = System.IO.Path.GetDirectoryName(file_path);
+                if(!System.IO.Directory.Exists(directory))
+                {
+                    System.IO.Directory.CreateDirectory(directory);
                 }
 
                 using (System.IO.FileStream file_stream = new FileStream(file_path, FileMode.Create))
@@ -51,13 +59,56 @@ namespace AnyCPU
                     dll_resource.Read(byte_array, 0, (int)dll_resource.Length);
 
                     file_stream.Write(byte_array, 0, byte_array.Length);
+
+                    // Since Managed C++ dlls also contained native code, the assembly must be loaded using a LoadFile!
                     //return Assembly.Load(byte_array);
                 }
             }
 
             Assembly assembly = Assembly.LoadFile(file_path);
+
+            // Unable to delete file while the assembly is loaded into the domain. 
             //File.Delete(file_path);
+
             return assembly;
+        }
+
+        internal static string GetInteropDLLPath(string resource_assembly)
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetAssembly(typeof(ModuleInitializer));
+
+            string local_directory = System.IO.Path.GetDirectoryName(assembly.Location);
+            if(IsWritable(local_directory))
+            {
+                return local_directory + Platform() + @"\" + resource_assembly;
+            }
+
+            string assembly_name = new AssemblyName(assembly.FullName).Name;
+
+            return System.Environment.GetEnvironmentVariable("TEMP") + @"\\" + assembly_name + @"\" + Platform() + @"\" + resource_assembly;
+        }
+
+        internal static bool IsWritable(string directory)
+        {
+            try
+            {
+                string test_file = System.IO.Path.GetRandomFileName();
+                using (System.IO.FileStream fs = System.IO.File.Create(directory + test_file))
+                {
+                    // do nothing
+                }
+                System.IO.File.Delete(test_file);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        internal static string Platform()
+        {
+            return (Environment.Is64BitProcess) ? "x64" : "x86";
         }
     }
 }
